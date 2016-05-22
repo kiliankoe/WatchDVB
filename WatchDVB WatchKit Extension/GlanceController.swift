852132cbf1d960f2b7eda2360c81ad32a62e421c
Kilian Koeltzsch <me@kilian.io>
2016-05-22 14:08:16 +0200

    Initial Commit

---
+++ WatchDVB WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  WatchDVB WatchKit Extension
+//
+//  Created by Kilian Költzsch on 22/05/16.
+//  Copyright © 2016 Kilian Koeltzsch. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
